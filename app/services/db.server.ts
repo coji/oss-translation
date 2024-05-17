7601c207ad225c3473d0fdd658dc290424ae5fa6
coji <coji@techtalk.jp>
2024-05-17 20:12:44 +0900

    chore: Update PrismaClient configuration for query logging

--- app/services/db.server.ts
+++ app/services/db.server.ts
@@ -1,3 +1,14 @@
 import { PrismaClient } from '@prisma/client'
+import createDebug from 'debug'
 
-export const db = new PrismaClient({})
+const debug = createDebug('app:db')
+
+export const db = new PrismaClient({
+  log: [{ emit: 'event', level: 'query' }],
+})
+
+db.$on('query', (e) => {
+  debug(`Query: ${e.query}`)
+  debug(`Params: ${e.params}`)
+  debug(`Duration: ${e.duration}ms`)
+})
