119e55da08233d549b2099207d4fcb6771bb643d
coji <coji@techtalk.jp>
2024-05-19 21:19:21 +0900

    chore: Add form validation and submission logic to NewProjectPage

--- app/routes/_app+/new/route.tsx
+++ app/routes/_app+/new/route.tsx
@@ -1,4 +1,15 @@
-import { Link } from '@remix-run/react'
+import {
+  getFormProps,
+  getInputProps,
+  getTextareaProps,
+  useForm,
+} from '@conform-to/react'
+import { parseWithZod } from '@conform-to/zod'
+import type { ActionFunctionArgs, LoaderFunctionArgs } from '@remix-run/node'
+import { Form, Link, useActionData, useLoaderData } from '@remix-run/react'
+import fs from 'node:fs/promises'
+import path from 'node:path'
+import { z } from 'zod'
 import {
   Button,
   Card,
@@ -13,9 +24,54 @@ import {
 } from '~/components/ui'
 import { Stack } from '~/components/ui/stack'
 
+const schema = z.object({
+  name: z.string(),
+  description: z.string().optional(),
+  path: z.string(),
+})
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const dir = await fs.readdir('./projects')
+  return { dir }
+}
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+  const submission = parseWithZod(await request.formData(), { schema })
+  if (submission.status !== 'success') {
+    return { lastResult: submission.reply() }
+  }
+
+  const stat = await fs
+    .stat(path.join('./projects', submission.value.path))
+    .catch(() => null)
+  if (!stat) {
+    return {
+      lastResult: submission.reply({
+        fieldErrors: { path: ['Path does not exist'] },
+      }),
+    }
+  }
+  if (!stat.isDirectory()) {
+    return {
+      lastResult: submission.reply({
+        fieldErrors: { path: ['Path must be a directory'] },
+      }),
+    }
+  }
+
+  return { lastResult: submission.reply() }
+}
+
 export default function NewProjectPage() {
+  const { dir } = useLoaderData<typeof loader>()
+  const actionData = useActionData<typeof action>()
+  const [form, { name, description, path }] = useForm({
+    lastResult: actionData?.lastResult,
+    onValidate: ({ formData }) => parseWithZod(formData, { schema }),
+  })
+
   return (
-    <form>
+    <Form method="post" {...getFormProps(form)}>
       <Card>
         <CardHeader>
           <CardTitle>New Project</CardTitle>
@@ -23,15 +79,32 @@ export default function NewProjectPage() {
         </CardHeader>
         <CardContent>
           <Stack>
-            <Label>
-              Name
-              <Input type="text" />
-            </Label>
+            <div>
+              <Label htmlFor={name.id}>Name</Label>
+              <Input {...getInputProps(name, { type: 'text' })} />
+              <div id={name.errorId} className="text-sm text-destructive">
+                {name.errors}
+              </div>
+            </div>
 
-            <Label>
-              <span>Project Description</span>
-              <Textarea />
-            </Label>
+            <div>
+              <Label htmlFor={description.id}>Description</Label>
+              <Textarea {...getTextareaProps(description)} />
+              <div
+                id={description.errorId}
+                className="text-sm text-destructive"
+              >
+                {description.errors}
+              </div>
+            </div>
+
+            <div>
+              <Label>Path</Label>
+              <Input {...getInputProps(path, { type: 'text' })} />
+              <div id={path.errorId} className="text-sm text-destructive">
+                {path.errors}
+              </div>
+            </div>
           </Stack>
         </CardContent>
         <CardFooter>
@@ -42,6 +115,6 @@ export default function NewProjectPage() {
           <Button type="submit">Create Project</Button>
         </CardFooter>
       </Card>
-    </form>
+    </Form>
   )
 }
