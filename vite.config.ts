922f43881b698c3f9921393070075ec831bef19d
coji <coji@techtalk.jp>
2024-12-11 20:59:18 +0900

    feat: Add routes configuration and update Vite to use safe-routes

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { vitePlugin as remix } from '@remix-run/dev'
-import { flatRoutes } from 'remix-flat-routes'
-import { remixRoutes } from 'remix-routes/vite'
+import { safeRoutes } from 'safe-routes/vite'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import { configDefaults } from 'vitest/config'
 
 declare module '@remix-run/server-runtime' {
   // or cloudflare, deno, etc.
@@ -18,20 +16,17 @@ export default defineConfig({
   },
   plugins: [
     remix({
-      routes: (defineRoutes) => flatRoutes('routes', defineRoutes),
       future: {
         v3_fetcherPersist: true,
         v3_throwAbortReason: true,
         v3_relativeSplatPath: true,
         v3_lazyRouteDiscovery: true,
         v3_singleFetch: true,
+        v3_routeConfig: true,
         unstable_optimizeDeps: true,
       },
     }),
-    remixRoutes(),
+    safeRoutes(),
     tsconfigPaths(),
   ],
-  test: {
-    exclude: [...configDefaults.exclude, 'projects'],
-  },
 })
