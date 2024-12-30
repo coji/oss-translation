150a5e456478e400fb78e6b1d5892a829c8ab3e3
coji <coji@techtalk.jp>
2024-12-30 21:37:51 +0900

    feat: Upgrade Vite to version 6.0.6 and enhance SSR conditions in configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 import { reactRouter } from '@react-router/dev/vite'
 import { safeRoutes } from 'safe-routes/vite'
-import { defineConfig } from 'vite'
+import {
+  defaultClientConditions,
+  defaultServerConditions,
+  defineConfig,
+} from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   optimizeDeps: { exclude: ['projects'] },
   plugins: [reactRouter(), safeRoutes(), tsconfigPaths()],
+  resolve: { conditions: [...defaultClientConditions] },
+  ssr: {
+    resolve: {
+      conditions: [...defaultServerConditions],
+      externalConditions: [...defaultServerConditions],
+    },
+  },
 })
