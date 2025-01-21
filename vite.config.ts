617491812e008724d9e0d7de1fe71595dc6c618b
coji <coji@techtalk.jp>
2025-01-21 23:58:37 +0900

    feat: Simplify Vite configuration by removing unused conditions

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,9 @@
 import { reactRouter } from '@react-router/dev/vite'
 import { safeRoutes } from 'safe-routes/vite'
-import {
-  defaultClientConditions,
-  defaultServerConditions,
-  defineConfig,
-} from 'vite'
+import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   optimizeDeps: { exclude: ['projects'] },
   plugins: [reactRouter(), safeRoutes(), tsconfigPaths()],
-  resolve: { conditions: [...defaultClientConditions] },
-  ssr: {
-    resolve: {
-      conditions: [...defaultServerConditions],
-      externalConditions: [...defaultServerConditions],
-    },
-  },
 })
